65f0493ac711ea9517805b3278bbe3d665acdb4d
jake-pauls <jakepauls99@gmail.com>
2022-03-03 00:33:48 -0800

    Created sheets integration using Google APIs

--- ext/src/components/PopupButton.jsx
+++ ext/src/components/PopupButton.jsx
@@ -1,60 +1,52 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-import Table from "./Table";
-import "./App.css";
+import Table from './Table';
+import SheetsButton from './SheetsButton';
 
-export default function PopupButton() {
-  // const popup = () => {
-  //   chrome.tabs.create(
-  //     {
-  //       url: 'popup/index.html',
-  //       active: false,
-  //     },
-  //     (tab) => {
-  //       chrome.windows.create({
-  //         tabId: tab.id,
-  //         type: 'popup',
-  //         focused: true,
-  //         height: 500,
-  //         width: 500,
-  //       });
-  //     }
-  //   );
-  // };
+import './App.css';
 
+export default function PopupButton({ apiKey }) {
   const [keywords, setKeywords] = useState('');
   const [response, setResponse] = useState([]);
 
   const sendKeywords = async () => {
     let parsedKeywords = keywords.split(',');
 
-    let body = { "keywords": parsedKeywords }
+    let body = { keywords: parsedKeywords };
 
-    const response = await axios.post("http://localhost:4040/api/recruit", body)  
+    const response = await axios.post(
+      'http://localhost:4040/api/recruit',
+      body
+    );
     console.log(response.data.recruits);
 
-    setResponse(response.data.recruits)
-  }
+    setResponse(response.data.recruits);
+  };
 
   const columns = React.useMemo(
     () => [
       {
-        Header: "Name",
-        accessor: "name"
+        Header: 'Name',
+        accessor: 'name',
       },
       {
-        Header: "Title",
-        accessor: "title"
+        Header: 'Title',
+        accessor: 'title',
       },
       {
-        Header: "Company",
-        accessor: "company"
+        Header: 'Company',
+        accessor: 'company',
       },
       {
-        Header: "Link",
-        accessor: "url",
-        Cell: e =><a href={e.value} target="_blank"> Link </a>
+        Header: 'Link',
+        accessor: 'url',
+        Cell: (e) => (
+          <a href={e.value} target="_blank">
+            {' '}
+            Link{' '}
+          </a>
+        ),
       },
     ],
     []
@@ -62,13 +54,27 @@ export default function PopupButton() {
 
   return (
     <>
-    <div class="container">
-      <input type="text" onChange={(e) => setKeywords(e.target.value)} placeholder="Search"></input>
-      <button class="button" onClick={sendKeywords}>Go</button>
-    </div>
-    <div class="container">
-      <Table columns={columns} data={response} />
-    </div>
+      <div class="container">
+        <input
+          type="text"
+          onChange={(e) => setKeywords(e.target.value)}
+          placeholder="Search"
+        ></input>
+        <button class="button" onClick={sendKeywords}>
+          Go
+        </button>
+      </div>
+      <div class="container">
+        { response !== undefined && response.length > 0 ? (
+          <>
+            <Table columns={columns} data={response} />
+            <SheetsButton apiKey={apiKey} data={response} />
+          </> )
+          : ( 
+            <></> 
+          )
+        }
+      </div>
     </>
   );
 }
---
+++ ext/src/components/SheetsButton.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+export default function SheetsButton({ apiKey, data }) {
+  const baseUrl = 'https://sheets.googleapis.com/v4/spreadsheets';
+  const columns = ['Name', 'Title', 'Company', 'Link'];
+  const jsonColumns = ['name', 'title', 'company', 'url'];
+
+  const initAuthToken = () => {
+    return new Promise((resolve) => {
+      chrome.identity.getAuthToken({ interactive: true }, (token) =>
+        resolve(token)
+      );
+    });
+  };
+
+  const pushDataToSheet = async () => {
+    let token = await initAuthToken();
+
+    let sheet = await createSheet(token);
+    await updateSheet(token, sheet);
+  };
+
+  // Create an empty sheet with a dated title and a basic 'Recruits' sheet
+  const createSheet = async (token) => {
+    const url = `${baseUrl}?key=${apiKey}`;
+
+    const date = new Date();
+    const formattedDate = `${date.getFullYear()}/${
+      date.getMonth() + 1
+    }/${date.getDate()}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+    const body = {
+      properties: {
+        title: `RapidRecruit Results ${formattedDate}`,
+      },
+      sheets: [
+        {
+          properties: {
+            title: 'Recruits',
+          },
+        },
+      ],
+    };
+
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    };
+
+    let response = await axios.post(url, body, { headers: headers });
+
+    return response.data;
+  };
+
+  const updateSheet = async (token, sheet) => {
+    const sheetId = sheet.spreadsheetId;
+    const url = `${baseUrl}/${sheetId}/values:batchUpdate?key=${apiKey}`;
+
+    // Append columns to first row of sheet
+    let sheetData = [columns];
+
+    // For each found recruit, add a row to the sheet
+    data.forEach((recruit) => {
+      let row = [];
+
+      jsonColumns.forEach((heading) => {
+        row.push(recruit[heading]);
+      });
+
+      sheetData.push(row);
+    });
+
+    const body = {
+      data: {
+        range: 'Recruits',
+        values: sheetData,
+      },
+      valueInputOption: 'USER_ENTERED',
+    };
+
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    };
+
+    let response = await axios.post(url, body, { headers: headers });
+    console.log(response);
+  };
+
+  return <button class="button" onClick={pushDataToSheet}>Export to Google Sheet</button>;
+}
